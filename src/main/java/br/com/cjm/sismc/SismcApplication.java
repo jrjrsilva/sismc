c5b91e143d491a6a501aab7eae0ea1c941dc658a
Silva <jrjrsilva@gmail.com>
2018-12-17 20:08:19 -0300

    criação do projeto

---
+++ src/main/java/br/com/cjm/sismc/SismcApplication.java
@@ -0,0 +1,14 @@
+package br.com.cjm.sismc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismcApplication.class, args);
+	}
+
+}
+
