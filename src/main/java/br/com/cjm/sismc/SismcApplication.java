b9755941f0f54164c8125707d141309cd4787dec
Silva <jrjrsilva@gmail.com>
2018-12-18 21:21:22 -0300

    produto e associação muitos para muitos

--- src/main/java/br/com/cjm/sismc/SismcApplication.java
+++ src/main/java/br/com/cjm/sismc/SismcApplication.java
@@ -11,13 +11,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cjm.sismc.domain.Categoria;
+import br.com.cjm.sismc.domain.Produto;
 import br.com.cjm.sismc.repositories.CategoriaRepository;
+import br.com.cjm.sismc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SismcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SismcApplication.class, args);
@@ -27,8 +32,17 @@ public class SismcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
