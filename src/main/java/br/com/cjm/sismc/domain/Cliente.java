dbe2c5ad80763dfdffff3ab600dffebd505be09e
Silva <jrjrsilva@gmail.com>
2018-12-20 20:45:41 -0300

    Endpont /pedidos/id

--- src/main/java/br/com/cjm/sismc/domain/Cliente.java
+++ src/main/java/br/com/cjm/sismc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.cjm.sismc.domain.enums.TipoCliente;
@@ -33,6 +34,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
