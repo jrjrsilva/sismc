b17f10e4f7ff415bc9dc0ba77b23f44bb1585a20
Silva <jrjrsilva@gmail.com>
2018-12-20 23:06:34 -0300

    Paginação com parametros

--- src/main/java/br/com/cjm/sismc/SismcApplication.java
+++ src/main/java/br/com/cjm/sismc/SismcApplication.java
@@ -73,6 +73,17 @@ public class SismcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		Categoria cat3 = new Categoria(null,"Escritorio 3");
+		Categoria cat4 = new Categoria(null,"Escritorio 4");
+		Categoria cat5 = new Categoria(null,"Escritorio 5");
+		Categoria cat6 = new Categoria(null,"Escritorio 6");
+		Categoria cat7 = new Categoria(null,"Escritorio 7");
+		Categoria cat8 = new Categoria(null,"Escritorio 8");
+		Categoria cat9 = new Categoria(null,"Escritorio 9");
+		Categoria cat10 = new Categoria(null,"Escritorio 10");
+		Categoria cat11 = new Categoria(null,"Escritorio 11");
+		Categoria cat12 = new Categoria(null,"Escritorio 12");
+		
 		
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
@@ -86,7 +97,7 @@ public class SismcApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11,cat12));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null,"Minas Gerais");
