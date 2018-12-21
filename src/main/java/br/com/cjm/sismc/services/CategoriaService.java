1cbd9a813aa4b0f47744455d52c4f02b68c78ddc
Silva <jrjrsilva@gmail.com>
2018-12-20 22:44:47 -0300

    Listando todas Categorias com DTO

--- src/main/java/br/com/cjm/sismc/services/CategoriaService.java
+++ src/main/java/br/com/cjm/sismc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.cjm.sismc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,8 @@ public class CategoriaService {
 		}		
 	}
 	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 
 }
