8af6d2f7aac69aeb9f10a37b6181d15e4af46b57
Silva <jrjrsilva@gmail.com>
2018-12-21 23:09:37 -0300

    inserindo cliente com post

--- src/main/java/br/com/cjm/sismc/resources/ClienteResource.java
+++ src/main/java/br/com/cjm/sismc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.cjm.sismc.domain.Cliente;
 import br.com.cjm.sismc.dto.ClienteDTO;
+import br.com.cjm.sismc.dto.ClienteNewDTO;
 import br.com.cjm.sismc.services.ClienteService;
 
 @RestController
@@ -35,7 +36,7 @@ public class ClienteResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDTO){
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
 		Cliente obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
--- src/main/java/br/com/cjm/sismc/services/ClienteService.java
+++ src/main/java/br/com/cjm/sismc/services/ClienteService.java
@@ -3,17 +3,22 @@ package br.com.cjm.sismc.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.loader.collection.OneToManyJoinWalker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.cjm.sismc.domain.Cidade;
 import br.com.cjm.sismc.domain.Cliente;
+import br.com.cjm.sismc.domain.Endereco;
+import br.com.cjm.sismc.domain.enums.TipoCliente;
 import br.com.cjm.sismc.dto.ClienteDTO;
+import br.com.cjm.sismc.dto.ClienteNewDTO;
 import br.com.cjm.sismc.repositories.ClienteRepository;
+import br.com.cjm.sismc.repositories.EnderecoRepository;
 import br.com.cjm.sismc.services.exception.DataIntegrityException;
 import br.com.cjm.sismc.services.exception.ObjectNotFoundException;
 
@@ -24,15 +29,21 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepo;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		enderecoRepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -61,8 +72,24 @@ public class ClienteService {
 		 return repo.findAll(pageRequest);
 	}
 	
-	public Cliente fromDTO(ClienteDTO objDTO) {
-		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null,objDto.getNome(),objDto.getEmail(),objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
+		Endereco end = new Endereco(null,objDto.getLogradouro(),objDto.getNumero(),objDto.getComplemento(),objDto.getBairro(),objDto.getCep(),cli,cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
