38f3a29144412254eb5ccc84c5911bb306ca58b1
Silva <jrjrsilva@gmail.com>
2018-12-22 19:55:50 -0300

    validação personalizada email insert cliente

--- src/main/java/br/com/cjm/sismc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/cjm/sismc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.cjm.sismc.domain.Cliente;
 import br.com.cjm.sismc.domain.enums.TipoCliente;
 import br.com.cjm.sismc.dto.ClienteNewDTO;
+import br.com.cjm.sismc.repositories.ClienteRepository;
 import br.com.cjm.sismc.resources.exception.FieldMessage;
 import br.com.cjm.sismc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	public void initialize(ClienteNewDTO ann) {
 	}
 	
@@ -26,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
