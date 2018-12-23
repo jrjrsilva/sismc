e2efb6e5727aa5ada8dc828316e2ffd90aec4dac
Silva <jrjrsilva@gmail.com>
2018-12-23 01:10:22 -0300

    criando profile teste

---
+++ src/main/java/br/com/cjm/sismc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package br.com.cjm.sismc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.cjm.sismc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	DBService dbService;
+	
+	@Bean
+	public boolean instatiateDatabase() throws ParseException {
+		dbService.instatiateTestDatabase();
+		return true;
+	}
+}
