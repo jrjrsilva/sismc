803338d26771beb5cf6212616b143a3e968202a3
Silva <jrjrsilva@gmail.com>
2018-12-25 19:20:02 -0300

    tentativa envio heroku

--- src/main/java/br/com/cjm/sismc/SismcApplication.java
+++ src/main/java/br/com/cjm/sismc/SismcApplication.java
@@ -3,17 +3,27 @@ package br.com.cjm.sismc;
 
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SismcApplication implements CommandLineRunner{
+@RestController
+public class SismcApplication extends SpringBootServletInitializer{
 
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SismcApplication.class);
+	    }
+
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(SismcApplication.class, args);
+	    }
 	
 	
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		SpringApplication.run(SismcApplication.class, args);
 	}
 
@@ -21,6 +31,6 @@ public class SismcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 	}
-
+*/
 }
 
