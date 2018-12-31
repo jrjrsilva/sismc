a966ccdbe4659a2132054d22d3db7ebdc9b084c5
Silva <jrjrsilva@gmail.com>
2018-12-31 18:06:24 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/br/com/cjm/sismc/dto/CredenciaisDTO.java
@@ -0,0 +1,37 @@
+package br.com.cjm.sismc.dto;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String senha;
+	
+	public CredenciaisDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CredenciaisDTO(String email, String senha) {
+		super();
+		this.email = email;
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
