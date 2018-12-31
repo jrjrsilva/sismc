819cb10188d904ee3eb05a49f44d4a6275103688
Silva <jrjrsilva@gmail.com>
2018-12-31 15:47:05 -0300

    Implementando Email HTML

--- src/main/java/br/com/cjm/sismc/services/MockEmailService.java
+++ src/main/java/br/com/cjm/sismc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package br.com.cjm.sismc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,6 +12,13 @@ public class MockEmailService extends AbstractEmailService{
 	
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de e-mail...");
+		LOG.info(msg.toString());
+		LOG.info("E-mail enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de e-mail HTML");
 		LOG.info(msg.toString());
 		LOG.info("E-mail enviado");
 	}
--- src/main/java/br/com/cjm/sismc/services/SmtpEmailService.java
+++ src/main/java/br/com/cjm/sismc/services/SmtpEmailService.java
@@ -1,15 +1,23 @@
 package br.com.cjm.sismc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import br.com.cjm.sismc.domain.Pedido;
 
 public class SmtpEmailService extends AbstractEmailService{
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	
@@ -18,6 +26,15 @@ public class SmtpEmailService extends AbstractEmailService{
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando e-mail...");
 		mailSender.send(msg);
+		LOG.info("E-mail enviado");		
+	}
+
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando e-mail...");
+		javaMailSender.send(msg);
 		LOG.info("E-mail enviado");		
 	}
 
